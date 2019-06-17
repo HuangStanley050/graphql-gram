d7b9c83af649f654a4f17dc07a79a66d19494720
Stanley Huang <huang.stanley050@gmail.com>
2019-06-17 15:38:35 +1000

    return all posts provided user have login and with a token

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,9 +1,24 @@
 import Post from "../models/post";
+import { checkAuth } from "../middlewares/auth";
+
 const query = {
   hello: (parent, args, ctx, info) => {
     return "hello";
   },
-  allposts: (parent, args, ctx, info) => {}
+  allposts: async (parent, args, { request }, info) => {
+    let decoded;
+    if (!request.headers.authorization) {
+      throw new Error("No auth in header");
+    }
+    const token = request.headers.authorization.split(" ")[1];
+    decoded = checkAuth(token);
+    if (decoded) {
+      let posts = await Post.find({});
+      return posts;
+    } else {
+      throw new Error("Not authenticated");
+    }
+  }
 };
 
 export default query;
