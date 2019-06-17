ac96e32c8269db30b105bbf247201711b62c4854
Stanley Huang <huang.stanley050@gmail.com>
2019-06-17 15:47:52 +1000

    find a single post by id

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,11 +1,24 @@
 import Post from "../models/post";
-import { checkAuth } from "../middlewares/auth";
+import {checkAuth} from "../middlewares/auth";
 
 const query = {
   hello: (parent, args, ctx, info) => {
     return "hello";
   },
-  allposts: async (parent, args, { request }, info) => {
+  singlePost: async (parent, args, {request}, info) => {
+    let decoded;
+    let postId = args.postId;
+    if (!request.headers.authorization) {
+      throw new Error("No auth in header");
+    }
+    const token = request.headers.authorization.split(" ")[1];
+    decoded = checkAuth(token);
+    if (decoded) {
+      let post = await Post.findById(postId);
+      return post;
+    }
+  },
+  allposts: async (parent, args, {request}, info) => {
     let decoded;
     if (!request.headers.authorization) {
       throw new Error("No auth in header");
